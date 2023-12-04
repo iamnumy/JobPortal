04475644c78a6d11515d7acb042614794da451c5
Nouman Noor <63315592+iamnumy@users.noreply.github.com>
2023-12-05 01:32:22 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ var corsOptions = {
 };
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://job-portal-api-chi.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
