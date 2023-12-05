41a59448e0d5aea47b9db9fce596dbe5bdcc8918
Nouman Noor <63315592+iamnumy@users.noreply.github.com>
2023-12-05 02:44:46 +0200

    Update Signup.js

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -61,7 +61,7 @@ const SignUp = () => {
         };
 
         try {
-            const response = await fetch('http://127.0.0.1:8080/api/auth/signup', {
+            const response = await fetch('https://job-portal-backend-two.vercel.app/api/auth/signup', {
                 method: 'POST',
                 body: new URLSearchParams(userData),
             });
